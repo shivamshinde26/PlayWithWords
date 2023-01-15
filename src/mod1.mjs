ca51c51e79b917637e6f83f2633e6f292fdd8697
shivamshinde26 <shivamshinde2663@gmail.com>
2023-01-15 20:03:58 +0530

    my first commit reactwebsite

---
+++ src/mod1.mjs
@@ -0,0 +1,4 @@
+import dza, { b, c } from './mod2.mjs'
+console.log(dza)
+console.log(b)
+console.log(c)
---
+++ src/mod2.mjs
@@ -0,0 +1,6 @@
+const a="shivam"
+const b="suresh"
+const c="shinde"
+export default a
+export  {b}
+export  {c}
